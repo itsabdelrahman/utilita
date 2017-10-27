3d008a0c18ee1cd61827932951a912ab31eb16bd
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-10-28 00:49:59 +0200

    :sparkles: Implement replaceAt()

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const merge = require('lodash.merge');
 const set = (object, property, value) =>
   merge({}, object, { [property]: value });
 
+const replaceAt = (array, index, thing) =>
+  array
+    .slice(0, index)
+    .concat(thing)
+    .concat(array.slice(index + 1));
+
 const randomString = length =>
   Math.random()
     .toString(36)
@@ -14,6 +20,7 @@ const logJSON = json => console.log(JSON.stringify(json, null, 2));
 
 module.exports = {
   set,
+  replaceAt,
   randomString,
   testRegex,
   logJSON,
