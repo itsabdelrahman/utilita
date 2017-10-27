9a8bd0b837f6d5b242a4f5838ea784a16d7bf1b1
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-10-28 00:16:35 +0200

    :hammer: Implement testRegex()

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ const set = (object, property, value) =>
 
 const logJSON = json => console.log(JSON.stringify(json, null, 2));
 
+const testRegex = (regex, string) => new RegExp(regex).test(string);
+
 module.exports = {
   set,
   logJSON,
+  testRegex,
 };
