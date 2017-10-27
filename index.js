8f1eea2c6b45924eaad2ca86ed66350a09acbfa7
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-10-28 00:08:02 +0200

    :sparkles: Implement logJSON()

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const merge = require('lodash.merge');
 const set = (object, property, value) =>
   merge({}, object, { [property]: value });
 
+const logJSON = json => console.log(JSON.stringify(json, null, 2));
+
 module.exports = {
   set,
+  logJSON,
 };
