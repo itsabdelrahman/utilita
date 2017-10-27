4b92bd4c1c276861daac269a2713379cfc7b2ac2
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-10-28 00:28:54 +0200

    :sparkles: Implement randomString()

--- index.js
+++ index.js
@@ -7,8 +7,14 @@ const logJSON = json => console.log(JSON.stringify(json, null, 2));
 
 const testRegex = (regex, string) => new RegExp(regex).test(string);
 
+const randomString = length =>
+  Math.random()
+    .toString(36)
+    .substr(2, length);
+
 module.exports = {
   set,
   logJSON,
   testRegex,
+  randomString,
 };
