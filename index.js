038914d394379db2ebf37849168fcae56a9904fc
A.R. Maged <abdulrahman.maged95@gmail.com>
2017-10-28 11:53:05 +0200

    :sparkles: Implement removeAt()

--- index.js
+++ index.js
@@ -9,6 +9,11 @@ const replaceAt = (array, index, thing) =>
     .concat(thing)
     .concat(array.slice(index + 1));
 
+const removeAt = (array, index) => ({
+  array: array.slice(0, index).concat(array.slice(index + 1)),
+  element: array[index],
+});
+
 const randomString = length =>
   Math.random()
     .toString(36)
@@ -21,6 +26,7 @@ const logJSON = json => console.log(JSON.stringify(json, null, 2));
 module.exports = {
   set,
   replaceAt,
+  removeAt,
   randomString,
   testRegex,
   logJSON,
